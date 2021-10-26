77d91b763cb97d6f0e89151df391127c83e5c3ba
unknown <juanpabloferroarroyo@gmail.com>
2021-10-26 05:17:28 +0200

    styling image-cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { Formik, Field, Form } from "formik";
 import { useState } from "react";
 import "./header.css";
+import "./content.css";
+import "./article.css";
 
 const App = () => {
   const [photos, setPhotos] = useState([]);
+  const open = (url) => {
+    window.open(url);
+  };
   console.log(photos);
   return (
     <div>
@@ -30,6 +35,16 @@ const App = () => {
           </Form>
         </Formik>
       </header>
+      <div className="container">
+        <div className="center">
+          {photos.map((photo) => (
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img src={photo.urls.regular} alt={photo.alt_description} />
+              <p>{[photo.description, photo.alt_description].join(" - ")}</p>
+            </article>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
